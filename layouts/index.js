36d25a0b0c1cc8c389695eeb21a06334d09ce6c0
Harland Duman <hduman@paypal.com>
2020-11-20 10:49:09 -0800

    Update style

--- layouts/index.js
+++ layouts/index.js
@@ -7,8 +7,8 @@ const Layout = ({ children, frontMatter }) => {
         <title>{frontMatter.title} - Harland Duman</title>
       </Head>
       <div className="font-medium text-gray-600">{frontMatter.date}</div>
-      <h1 className="mb-2 text-3xl font-semibold">{frontMatter.title}</h1>
-      <div className="w-32 mb-8 border-b-4 border-gray-300 sm:w-48" />
+      <h1 className="mb-8 text-3xl font-semibold ">{frontMatter.title}</h1>
+      <div className="w-16 mb-8 border-b-2 border-light-blue-500" />
       <div className="max-w-3xl mx-auto prose prose-lg">{children}</div>
     </div>
   );
